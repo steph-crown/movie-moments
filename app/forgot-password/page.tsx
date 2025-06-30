3391fb4d4ee902e9d729d849335d86613c1515f7
Steph Crown <emmanuelstephen024@gmail.com>
2025-06-30 18:41:05 +0100

    Create auth pages

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+export default function ForgotPassword() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      {/* Add your forgot password form here */}
+    </div>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Log In</h1>
+      {/* Add your login form here */}
+    </div>
+  );
+}
