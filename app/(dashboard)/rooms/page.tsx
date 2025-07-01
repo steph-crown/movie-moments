b94885f54ebf52968a3499a70efa3c1f35321756
Steph Crown <emmanuelstephen024@gmail.com>
2025-07-01 01:18:46 +0100

    Add popover component

--- app/(dashboard)/rooms/page.tsx
+++ app/(dashboard)/rooms/page.tsx
@@ -1,3 +1,34 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDown } from "lucide-react";
+
 export default function Rooms() {
-  return <div>rooms</div>;
+  return (
+    <div>
+      <div className="flex justify-end gap-4">
+        <div className="flex gap-8">
+          <Popover>
+            <PopoverTrigger className="text-xs font-medium flex gap-1 items-center">
+              All rooms
+              <ChevronDown className="h-4" />
+            </PopoverTrigger>
+
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+
+          <Popover>
+            <PopoverTrigger className="text-xs font-medium flex gap-1 items-center">
+              Last created
+              <ChevronDown className="h-4" />
+            </PopoverTrigger>
+
+            <PopoverContent>Place content for the popover here.</PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
 }
