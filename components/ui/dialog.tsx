7acbd964934142d61d686e12fc0f2744d17d315b
Steph Crown <emmanuelstephen024@gmail.com>
2025-07-01 18:13:46 +0100

    Update

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -64,7 +64,7 @@ function DialogContent({
         className={cn(
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border py-6 px-5 sm:p-6 shadow-lg duration-200 sm:max-w-lg max-h-[90vh] overflow-y-auto",
           fullScreenOnMobile &&
-            "max-[400px]:max-w-full max-[400px]:rounded-none  !max-h-full",
+            "max-[400px]:max-w-full max-[400px]:rounded-none  max-[400px]:max-h-full",
           className
         )}
         {...props}
